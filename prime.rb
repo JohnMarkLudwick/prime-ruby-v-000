e507c7b1297b3bae2ee86131679a4ef8e8b40448
JohnMark <johnmarkludwick@gmail.com>
2018-06-13 17:28:26 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,8 @@
 # Add  code here!
 
 def is_prime?(num)
-  return false if num <= 1
-  Math.sqrt(num).to_i.downto(2).each {|i| return false if num % i == 0}
-  true
+  return if num <= 1
+  (2..Math.sqrt(num)).none? { |i| (num % i).zero? }
 end
 
 
