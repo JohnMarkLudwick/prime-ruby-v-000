aedb5cdd060c8619da3990c88147f2daa2109638
JohnMark <johnmarkludwick@gmail.com>
2018-06-13 17:55:34 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -2,13 +2,10 @@
 
 require 'prime'
 
-def prime?(2)
-  if Prime.prime?
-    true
-  else
-    false
-  end
-
+def is_prime?(num)
+  return false if num <= 1
+  Math.sqrt(num).to_i.downto(2).each {|i| return false if num % i == 0}
+  true
 end
 
 
